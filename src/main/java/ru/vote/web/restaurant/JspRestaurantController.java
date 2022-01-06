1d37f7f00858af7183ec6dd7f84ddb5d7763d041
Vitalik Zhukov <unicraft1989@gmail.com>
2022-01-06 18:11:23 +0300

    Change Controller to RestController in RestaurantRestController

--- src/main/java/ru/vote/web/restaurant/JspRestaurantController.java
+++ src/main/java/ru/vote/web/restaurant/JspRestaurantController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Objects;
 
 @Controller
-@RequestMapping("restaurants")
+@RequestMapping("/restaurants")
 public class JspRestaurantController extends AbstractRestaurantController{
 
     @Autowired
--- src/main/java/ru/vote/web/restaurant/RestaurantRestController.java
+++ src/main/java/ru/vote/web/restaurant/RestaurantRestController.java
@@ -1,8 +1,65 @@
 package ru.vote.web.restaurant;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import ru.vote.model.Restaurant;
+
+import java.net.URI;
+import java.util.List;
 
 @Controller
+@RequestMapping(value = RestaurantRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 public class RestaurantRestController  extends AbstractRestaurantController{
+    static final String REST_URL = "/rest/profile/restaurants";
 
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Restaurant> createWithLocation(@RequestBody Restaurant restaurant) {
+        Restaurant created = super.create(restaurant);
+
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @Override
+    @GetMapping("/{id}")
+    public Restaurant get(@PathVariable int id) {
+        return super.get(id);
+    }
+
+    @Override
+    @GetMapping
+    public List<Restaurant> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@RequestBody Restaurant restaurant, @PathVariable int id) {
+        super.update(restaurant, id);
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id) {
+        super.delete(id);
+    }
+
+    @Override
+    public void incrementVoteCounter(int id) {
+        super.incrementVoteCounter(id);
+    }
+
+    @Override
+    public int getVoteCounter(int id) {
+        return super.getVoteCounter(id);
+    }
 }

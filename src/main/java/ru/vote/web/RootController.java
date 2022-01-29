58ace78277496fcc985989737b8ca86c608829eb
Vitalik Zhukov <unicraft1989@gmail.com>
2022-01-29 18:55:20 +0300

    Add form for logging

--- src/main/java/ru/vote/web/RootController.java
+++ src/main/java/ru/vote/web/RootController.java
@@ -6,12 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import ru.vote.service.MenuService;
 import ru.vote.service.RestaurantService;
-import ru.vote.util.SecurityUtil;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class RootController {
@@ -26,7 +22,7 @@ public class RootController {
     @GetMapping("/")
     public String root() {
         log.info("root");
-        return "index";
+        return "redirect:restaurants";
     }
 
     @GetMapping("/users")
@@ -49,11 +45,9 @@ public class RootController {
         return "menus";
     }
 
-    @PostMapping("/users")
-    public String setUser(HttpServletRequest request) {
-        int userId = Integer.parseInt(request.getParameter("userId"));
-        log.info("root setUser {}", userId);
-        SecurityUtil.setAuthUserId(userId);
-        return "redirect:restaurants";
+    @GetMapping("/login")
+    public String login() {
+        log.info("login");
+        return "login";
     }
 }
